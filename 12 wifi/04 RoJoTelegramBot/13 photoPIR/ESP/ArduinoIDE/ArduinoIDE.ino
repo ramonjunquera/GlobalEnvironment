bb1dfde160ef29b157e281fef7e2dc40bc8bd576
Ramón Junquera <ramonjunquera@users.noreply.github.com>
2018-10-29 19:00:31 +0100

    20181029

--- 12 wifi/04 RoJoTelegramBot/13 photoPIR/ESP/ArduinoIDE/ArduinoIDE.ino
+++ 12 wifi/04 RoJoTelegramBot/13 photoPIR/ESP/ArduinoIDE/ArduinoIDE.ino
@@ -1,32 +1,98 @@
 /*
   Autor: Ramón Junquera
-  Versión: 20181007
+  Versión: 20181029
   Tema: Librería para gestión de bots en Telegram
   Objetivo: Enviar una imagen al detectar movimiento
   Material: placa ESP, ArduCAM-Mini-2MP, PIR sensor
   Descripción:
-    Utilizando los ejemplos anteriores para como base.
-    Crearemos un sistema de suscripción segura.
-    Usamos el sistema de comprobación a intervalos variables.
-    Cuando el sensor detecte movimiento, automáticamente se enviará una foto en la 
-    resolución actual a todos los usuarios suscritos.
-    Se añade la posibilidad de desactivar manualmente el sensor de movimiento.
-    Se añaden capacidades OTA para facilitar la actualización.
-  Notas:
-  - Es ESP8266 es posible que haya que desconectar el pin D4 para tranferir el programa. Después se 
+    Sistema de videovigilancia con control por Telegram
+    El sistema consta de una placa ESP, una cámara y un sensor infrarrojo de movimiento.
+  Configuración del la transferencia:
+    Puesto que no tenemos limitación de alimentación, usaremos la máxima frecuencia de
+    reloj en el procesador.
+    La configuración de memoria por defecto es de 1Mb para programa y 3Mb para SPIFFS.
+    Transferir tanto la aplicación como los datos.
+  Transferencia por cable:
+    En ESP8266 es posible que haya que desconectar el pin D4 para tranferir el programa. Después se 
     puede volver a conectar al PIR.
-  - Si utilizamos la transferencia OTA es importante seleccionar la opción
-      Herramientas/Erase Flash/Sketch + WiFi Settings
+  Transferencia por OTA:
+    La aplicación tiene en cuenta la constante global devicePassword como contraseña de OTA.
+    Es importante seleccionar la opción: Herramientas/Erase Flash/Sketch + WiFi Settings
     Esto es porque estamos utilizando contraseñas para OTA y podrían quedarse los parámetros de la
     configuración previa.
-  - Puesto que el dispositivo tendrá alimentación garantizada, se puede utilizar la frecuencia más
-    alta de la CPU.
+    Recordar que si queremos actualizar datos, no podremos hacerlo con contraseña.
+    Se tendría que transferir una nueva apliación sin contraseña OTA para después actualizar los
+    datos por OTA.
+    La transferencia se puede hacer tanto a la dirección IP del punto de acceso que da conexión
+    a Internet como a través de la dirección 192.168.4.1 del punto de acceso que se crea para
+    configuración.
+  Configuración del programa:
+    El dispositivo crea un punto de acceso wifi con el nombre definido en la constante global
+    mySSID y contraseña en mySSIDpassword.
+    Nos conectamos y accedemos a la URL http://192.168.4.1
+    Se mostrará la configuración actual (inicialmente vacía).
+    Se puede cambiar el nombre y contraseña del punto de acceso wifi que le proporcionará la
+    salida a internet.
+    También se configura el token del bot de Telegram.
+    Por último se solicita la contraseña del dispositivo, definida en la constante global
+    devicePassword.
+    Si todo es correcto, se guarda la configuración y se reinicia el dispositivo.
+  Primer uso:
+    En Telegram debemos buscar el nombre del bot.
+    La primera vez no hay definidos usuarios.
+    El primer usuario puede suscribirse a sí mismo. A partir de entonces, el resto tendrán que 
+    ser invitados por alguno de los suscriptores.
+  Inicio de conversación con el bot:
+    Para iniciar cualquier bot se escribe el comando /start
+    A partir de aquí se mostrarán las opciones en menús (botones).
+  Sistema de detección de movimiento:
+    Tras el primer arranqe está desactivado.
+    Se puede cambiar el estado con los botones /on y /off.
+    Cuando se cambia el estado, se informa a todos los suscriptores.
+    Cuando está activado y el sensor infrarrojo detecta movimiento, se toma una fotografía
+    y se envía a todos los suscriptores.
+    El tiempo de envío depende de la resolución de la imágen.
+  Fotografía forzada:
+    Con independencia se si está activo o no el sistema de detección de movimiento, siempre se
+    puede forzar a tomar una imágen con el botón /photo. Esta acción no informará al resto
+    de suscriptores.
+  Corte de alimentación:
+    Si el dispositivo sufre un corte de alimentación, la próxima vez que reinicie enviará un
+    mensaje a todos los suscriptores informando y recuperará la última configuración utilizada.
+  Tiempo de reacción a comandos:
+    El tiempo de respuesta a un comando es variable. Se va alargando si no recibe nuevas 
+    peticiones, hasta alcanzar un máximo de 30 segundos.
+  Solicitud de estado:
+    El botón /status informa del estado del sistema de detección de movimiento.
+  Solicitud de resolución:
+    El botón /res informa de la resolución con la que se están tomándo las imágenes.
+  Cambio de resolución:
+    El botón /resX abre un nuevo menú de 9 opciones para seleccionar la nueva resolución.
+    La /res0 es la más baja y las /res9 la más alta.
+  Listado de suscriptores:
+    Se puede solicitar el listado de suscriptores a través de botón /users y botón /list
+  Desuscribirse:
+    Un suscriptor puede darse de baja él mismo a través de botón /users y botón /unsuscribe.
+    Si era el último, el sistema quedará abierto, puesto que el siguiente que acceda podrá darse
+    de alta a sí mismo sin la ayuda de nadie más.
+  Añadir un nuevo suscriptor:
+    Los nuevos suscriptores (invitado) siempre deben ser invitados por alguien suscrito (anfitrión).
+    El anfitrión debe generar un código a través del botón /users y botón /generate.
+    El código tiene una validez de 60 seundos. Antes de este tiempo el invitado debe enviar al bot
+    el comando /subscribe seguido del código.
+  Chat:
+    Si un suscriptor envía al bot mensajes que no son reconocidos como comandos, se supondrá que 
+    es texto normal y se le enviará el mensaje a todos los suscriptores.
 */
 #include <Arduino.h>
 #ifdef ESP8266 //Si es una ESP8266...
   #include <ESP8266WiFi.h> //Librería para gestión de wifi para ESP8266
+  #include <ESP8266WebServer.h> //Gestión de servidor web
+  #include <FS.h> //Gestión de SPIFFS
 #elif defined(ESP32)
   #include <WiFi.h> //Librería para gestión de wifi para ESP32
+  #include <WebServer.h> //Gestión de servidor web
+  #include <SPIFFS.h> //Gestión de SPIFFS
 #endif
 #include "RoJoTelegramBot.h" //Librería para gestión de bots de Telegram
 #include "RoJoArduCAM.h" //Librería de gestión de la cámara
@@ -34,16 +100,13 @@
 #include <ArduinoOTA.h> //Para gestión de OTA
 
 //Definición de constantes globales
-const char ssid[]="xxx"; //Nombre del punto de acceso (SSID)
-const char password[]="xxx"; //Contraseña
-const String botToken="xxx";
 const uint32_t checkingGap=1000; //Tiempo de espera en milisegundos para comprobación de nuevos mensajes
 const byte pinLed=LED_BUILTIN; //Pin del led integrado en placa
 const bool ledON=LOW; //Estado del pin del led cuando está encendido
 const uint32_t maxWait=30000; //Tiempo máximo de espera = 30 segundos
 const float factorWait=1.2; //Factor de la progresión geométrica para calcular el siguiente tiempo de espera
-bool enabled = true; //Por defecto, cuando arranca, se enciende
-byte currentRes=3; //Resolución actual
+bool enabled = false; //Por defecto, cuando arranca, está desactivado
+byte currentRes=3; //Resolución por defecto
 #ifdef ESP8266 //Si es una ESP8266...
   const byte pinPIR=D3; //Pin del sensor PIR
   const byte pinCS=D0; //Pin CS de la cámara
@@ -51,6 +114,17 @@ byte currentRes=3; //Resolución actual
   const byte pinPIR=2; //Pin del sensor PIR
   const byte pinCS=4; //Pin CS de la cámara
 #endif
+//Teclado por defecto
+const String defaultKeyb="[[\"/on\",\"/off\",\"/status\"],[\"/photo\",\"/users\"],[\"/res\",\"/resX\"]]";
+//Nombre del archivo que guarda el estado
+const String statusFile="/status.txt";
+//Definimos nombre del punto de acceso wifi y su contraseña (para cambiar la configuración)
+const String mySSID="RoJoPhotoPIR";
+const String mySSIDpassword="12345678";
+//Contraseña del dispositivo. Utilizada para:
+//- Cambiar la configuración
+//- Actualizar por OTA
+const String devicePassword="xxx";
 
 //Definición de variables globales
 //Creamos el objeto de gestión del bot
@@ -69,12 +143,105 @@ uint32_t subscriptionCodeTimeout=0;
 RoJoArduCAM camera;
 //Flag de deteccción de movimiento. Inicialmente no.
 bool movementDetected=false;
-//Creamos objeto de gestión de diccionarios
+//Creamos objeto de gestión de diccionarios en archivo
 RoJoFileDictionary subscribers;
-//Teclado por defecto
-const String defaultKeyb="[[\"/on\",\"/off\",\"/status\"],[\"/photo\",\"/users\"],[\"/res\",\"/resX\"]]";
-//Nombre del archivo que guarda el estado
-const String statusFile="/status.txt"; 
+//Creamos un servidor web en el puerto 80 para cambiar la configuración
+//La ip del servidor es fija en el punto de acceso propio: 192.168.4.1
+//La URL para cambiar la dirección será: http://192.168.4.1
+#ifdef ESP8266 //Si es una ESP8266...
+  ESP8266WebServer server(80);
+#elif defined(ESP32)
+  WebServer server(80);
+#endif
+String wifiClientSSID; //Nombre del punto de acceso (SSID) al que nos conectamos como clientes
+String wifiClientPassword; //Contraseña de punto de acceso al que nos conectamos como clientes
+String botToken; //Token del bot de Telegram ToniCamera1
+
+String htmlConfig()
+{
+  //Compone un texto HTML con la página de configuración según la siguiente plantilla
+  //<html>
+  //<h1>photoPIR configuration</h1>
+  //<p><label>Wifi client SSID:<input id="wifiSSID" type="text" name="SSID" value="var1"/></label></p>
+  //<p><label>Wifi client password:<input id="wifiPassword" type="text" name="password" value="var2" /></label></p>
+  //<p><label>Telegram bot token:<input id="botToken" type="text" name="botToken" value="var3" size="50"/></label></p>
+  //<p><label>device Id:<input id="devicePassword" type="text" name="devicePassword" /></label></p>
+  //<p><a href="?" onclick="location.href=this.href+'p0='+wifiSSID.value+'&p1='+wifiPassword.value+'&p2='+botToken.value+'&p3='+devicePassword.value;return false;"><button>set</button></a>
+  //</html>
+
+  //Se sustituyen los valores de los campos por los valores de la configuración actual
+  String answer="<html><h1>photoPIR configuration</h1>";
+  answer+="<p><label>Wifi client SSID:<input id=\"wifiSSID\" type=\"text\" name=\"SSID\" value=\"" + wifiClientSSID +"\"/></label></p>";
+  answer+="<p><label>Wifi client password:<input id=\"wifiPassword\" type=\"text\" name=\"password\" value=\"" + wifiClientPassword +"\" /></label></p>";
+  answer+="<p><label>Telegram bot token:<input id=\"botToken\" type=\"text\" name=\"botToken\" value=\"" + botToken + "\" size=\"50\"/></label></p>";
+  answer+="<p><label>device Id:<input id=\"devicePassword\" type=\"text\" name=\"devicePassword\" /></label></p>";
+  answer+="<p><a href=\"?\" onclick=\"location.href=this.href+'p0='+wifiSSID.value+'&p1='+wifiPassword.value+'&p2='+botToken.value+'&p3='+devicePassword.value;return false;\"><button>set</button></a></html>";
+  //Devolvemos la cadena HTML
+  return answer;
+}
+
+void serverHandleRoot()
+{
+  //Se ha solicitado la página web raíz
+
+  //Para que un cambio de configuración sea aceptado se debe cumplir que al menos tenga
+  //cuatro parámetros de nombres p0, p1, p2 y p3.
+  //Que el valor del parámetro p4 coincida con el identificador del dispositivo (deviceId)
+  //Variable para anotar si tiene todos los parámetros. Inicialmente no tienen ninguno
+  byte paramCounter=0;
+  //Array en el que guardaremos los parámetros ordenados
+  String params[4];
+  //Recorremos todos los argumentos recibidos...
+  for(byte i=0;i<server.args();i++)
+  {
+    //...si el parámetro es reconocido...
+    if(server.argName(i)=="p0")
+    {
+      paramCounter|=1; //Anotamos que tenemos el parámetro 0 (bit 0) 2^0
+      params[0]=server.arg(i); //Anotamos el parámetro
+    }
+    else if(server.argName(i)=="p1")
+    {
+      paramCounter|=2; //Anotamos que tenemos el parámetro 1 (bit 1) 2^1
+      params[1]=server.arg(i); //Anotamos el parámetro
+    }
+    else if(server.argName(i)=="p2")
+    {
+      paramCounter|=4; //Anotamos que tenemos el parámetro 2 (bit 2) 2^2
+      params[2]=server.arg(i); //Anotamos el parámetro
+    }
+    else if(server.argName(i)=="p3")
+    {
+      paramCounter|=8; //Anotamos que tenemos el parámetro 3 (bit 3) 2^3
+      params[3]=server.arg(i); //Anotamos el parámetro
+    }
+  }
+  //Hemos procesado todos los parámetros recibidos
+  //Si no se han recibido todos los parámetros necesarios...
+  //...o el parámetro 3 (devicePassword) no coincide...
+  //...devolvemos de nuevo la misma página de configuración
+  if(paramCounter<15 || params[3]!=devicePassword) server.send(200,"text/html",htmlConfig());
+  else //Si la configuración es correcta...
+  {
+    //...cambiamos los valores de las variables
+    wifiClientSSID=params[0]; //Parámetro 0 = SSID
+    wifiClientPassword=params[1]; //Parámetro 1 = password
+    botToken=params[2]; //Parámetro 2 = botToken;
+    //Guardaremos los valores actuales en el archivo de configuración
+    saveStatus();
+    //Devolvemos la página de cambio de configuración correcto
+    //La página se base en el HTML:
+    //<html>
+    //<h1>photoPIR configuration applied!</h1>
+    //<h2>restarting...</h2>
+    //</html>
+    server.send(200,"text/html","<html><h1>photoPIR configuration applied!</h1><h2>restarting...</h2></html>");
+    //Esperamos un momento
+    delay(1000);
+    //Reiniciamos el dispositivo para que conecte con la nueva configuración
+    ESP.restart();
+  }
+}
 
 void try2connect()
 {
@@ -82,11 +249,21 @@ void try2connect()
   //Si lo consigue, devuelve el control.
   //Si no lo consigue en menos de 10 segundos, resetea y comienza de nuevo.
 
-  //Intentamos conectar a wifi
-  //Nos conectaremos a un punto de acceso
-  WiFi.mode(WIFI_STA);
+  //Configuramos el wifi en modo mixto
+  //Se conectará a un punto de acceso y al mismo tiempo creará su propio punto de acceso
+  //La conexión cliente servirá para salir a Internet
+  //La conexión servidor servirá para cambiar la configuración
+  WiFi.mode(WIFI_AP_STA);
+  //Configuramos el punto de acceso wifi propio
+  //Necesitamos convertir los Strings en arrays de char
+  WiFi.softAP(mySSID.c_str(),mySSIDpassword.c_str()); //Nombre y contraseña
+  //Indicamos la función a la que se debe llamar cuando se solicite la URL raíz
+  server.on("/",serverHandleRoot);
+  //Arrancamos el servidor web para cambio de configuración
+  server.begin();
   //Inicializamos conexión wifi con las credenciales
-  WiFi.begin(ssid,password);
+  //Necesitamos convertir los Strings en arrays de char
+  WiFi.begin(wifiClientSSID.c_str(),wifiClientPassword.c_str());
   //Configuramos el pin del led integrado como salida
   pinMode(pinLed,OUTPUT);
   //Variable para contar las veces que repetimos el bucle
@@ -117,12 +294,30 @@ void saveStatus()
 
   //Abrimos el archivo del estado para escritura
   File f=SPIFFS.open(statusFile,"w");
-  //Guardamos la resolución actual en los 4 primeros bits y el estado en el bit 5
-  byte statusCode=currentRes;
-  if(enabled) statusCode+=0b00010000;
-  f.write(statusCode);
+  //Línea 1: resolución
+  f.println(currentRes);
+  //Línea 2: estado
+  f.println(enabled);
+  //Línea 3: SSID
+  f.println(wifiClientSSID);
+  //Línea 4: WiFi password
+  f.println(wifiClientPassword);
+  //Línea 5: botToken
+  f.println(botToken);
   //Cerramos el archivo
   f.close();
+}
+
+String readLine(File f)
+{
+  //Lee una línea de texto de un archivo abierto
+  //La línea debe finalizar con un \n
+
+  //Leemos la línea
+  String txt=f.readStringUntil('\n');
+  //Le quitamos el último carácter (el \n)
+  //Y devolvemos la respuesta
+  return txt.substring(0,txt.length()-1);
 }
 
 void readStatus()
@@ -141,17 +336,28 @@ void readStatus()
     f=SPIFFS.open(statusFile,"r");
   }
   //Tenemos el archivo abierto
-  
-  //Leemos el estado guardado
-  byte statusCode=f.read();
-  //Cerramos el archivo
-  f.close();
-  //Extraemos la resolución
-  currentRes=statusCode & 0b00001111;
+
+  //El archivo debe tener 5 líneas
+  Serial.println("Leyendo archivo de configuración:");
+  //  línea 1: resolución
+  currentRes=readLine(f).toInt();
+  Serial.println("currentRes="+String(currentRes));
   //Aplicamos la resolución
   camera.setResolution(currentRes);
-  //Extraemos el estado
-  enabled=statusCode & 0b00010000;
+  //  línea 2: estado
+  enabled=readLine(f).toInt();
+  Serial.println("enabled="+String(enabled));
+  //  línea 3: SSID
+  wifiClientSSID=readLine(f);
+  Serial.println("wifiClientSSID="+wifiClientSSID);
+  //  línea 4: password
+  wifiClientPassword=readLine(f);
+  Serial.println("wifiClientPassword="+wifiClientPassword);
+  //  línea 5: botToken
+  botToken=readLine(f);
+  Serial.println("botToken="+botToken);
+  //Cerramos el archivo
+  f.close();
 }
 
 void broadcast(String text)
@@ -489,6 +695,10 @@ void setup()
 {
   //Activamos el puerto serie para mensajes de debug
   Serial.begin(115200);
+  //Inicializamos el sistema de archivos
+  SPIFFS.begin();
+  //Leemos la última configuración guardada
+  readStatus();
   //Inicializamos el bot
   bot.begin(botToken);
   //Configuramos el pin del led como salida
@@ -529,8 +739,6 @@ void setup()
   subscribers.begin("/subscribers.txt");
   //Inicializamos la semilla de números aleatorios
   randomSeed(millis());
-  //Leemos la última configuración guardada
-  readStatus();
 
   //Definimos las funciones de OTA
   ArduinoOTA.onStart([]()
@@ -599,7 +807,8 @@ void setup()
   //Asignamos una contraseña para impedir que cualquiera pueda actualizarlo
   //No es obligatorio
   //Atención!. Es incompatible con el plugin de subida de archivos a SPIFFS
-  ArduinoOTA.setPassword("xxx");
+  //Si tenemos contraseña...la fijamos
+  if(devicePassword.length()>0) ArduinoOTA.setPassword(devicePassword.c_str());
   //Asignamos un nombre al dispositivo para tenerlo identificado
   //Si no lo hacemos tomará como nombre por defecto: "esp8266-[ChipID]"
   ArduinoOTA.setHostname("PhotoPIR");
@@ -613,10 +822,12 @@ void setup()
 
 void loop()
 {
-  //Procesa todos los nuevos mensajes
+  //Procesa todos los nuevos mensajes recibidos de Telegram
   handleNewMessages();
   //Comprobamos si hay alguna petición de actualización por OTA
   ArduinoOTA.handle();
+  //Refresca las conexiones del servidor web
+  server.handleClient();
   //Calculamos el tiempo máximo de espera
   uint32_t maxTime=millis()+currentWait;
   //Mientras no lleguemos al máximo tiempo de espera...
